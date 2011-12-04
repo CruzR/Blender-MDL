76ed8449c75c473944677c73d8163c8f94be66fe
Thomas Glamsch (CruzR) <thomas-glamsch@gmx.de>
2011-12-05 00:26:49 +0100

    Basic mesh import implemented. Currently only works for models with just one geoset.

--- WarMDLImport.py
+++ WarMDLImport.py
@@ -1,9 +1,13 @@
 import bpy
 import string
 import io
+import pdb
 
 from bpy_extras.io_utils import ImportHelper
 from bpy.props import StringProperty
+from bpy_extras import object_utils
+
+dbg = True
 
 # TODO: Add addon description
 # TODO: Add more comments
@@ -14,7 +18,8 @@ class StateMachine:
 # @param handlers: Dictionary containing name => function pairs
 # @param startState: The first state to call when starting
 # @param endStates: State machine will end execution on these
-	def __init__(self, handlers={}, startState=None, endStates=[]):
+	def __init__(self, parent, handlers={}, startState=None, endStates=[]):
+		self.parent = parent
 		self.handlers = handlers
 		self.startState = startState
 		self.endStates = endStates
@@ -26,7 +31,7 @@ class StateMachine:
 	def add(self, name, handler, endState=False, startState=False):
 		name = name.upper()
 		if handler:
-			self.handlers[name] = handler()
+			self.handlers[name] = handler(self.parent)
 		if endState:
 			self.endStates.append(name)
 			print(self.endStates)
@@ -60,9 +65,12 @@ class StateMachine:
 # They should always call newState, cargo = BaseHandler.run(cargo)
 # before doing anything else.
 class BaseHandler:
+	def __init__(self, parent):
+		self.parent = parent
+	
 	def run(self, cargo):
 		cargo['prev_handler'] = self.__class__.__name__
-		#print("Prev: {}".format(cargo['prev_handler']))
+		print(cargo['prev_handler'])
 		return ['SEARCH', cargo]
 
 class Geoset:
@@ -71,58 +79,53 @@ class Geoset:
 		self.normals = normals
 		self.tvertices = tvertices
 		self.faces = faces
-
-
-infile = None
-globalkeys = ['Version', 'Geoset']
-geosetkeys = ['Vertices', 'Normals', 'TVertices', 'Faces']
-geosets = []
-
 class SEARCH(BaseHandler):
 	def run(self, cargo):
 		newState, cargo = BaseHandler.run(self, cargo)
-		print('SEARCH')
+		#print('SEARCH')
 		while True:
-			cargo['last'] = current = infile.readline()
+			cargo['last'] = current = self.parent.infile.readline()
 			if current == '' :
 				newState = 'EOF'
 				break
 			elif current.strip().startswith('//'):
 				continue
-			elif current.strip().split()[0] in globalkeys:
+			elif current.strip().split()[0] in self.parent.globalkeys:
 				newState = current.strip().split()[0].upper()
 				break
 	
-		print('GOTO {}'.format(newState))
+		#print('GOTO {}'.format(newState))
 		return [newState, cargo]
 
 class VERSION(BaseHandler):
 	def run(self, cargo):
 		cargo = BaseHandler.run(self, cargo)[1]
-		print('VERSION')
-		if int(infile.readline().strip().strip(',').split()[1]) != 800:
+		#print('VERSION')
+		if int(self.parent.infile.readline().strip().strip(',').split()[1]) != 800:
 			raise Exception("This MDL Version is not supported!")
 		return ['SEARCH', cargo]
 
 class GEOSET(BaseHandler):
 	def run(self, cargo):
-		print('GEOSET')
+		#print('GEOSET')
 		if cargo['prev_handler'] == 'SEARCH':
 			try:
 				cargo['geoindex'] += 1
+				print('++')
 			except:
 				cargo['geoindex'] = 0
-			geosets.append(Geoset())
+			g = Geoset()
+			self.parent.geosets.append(g)
 			cargo['p'] = 1
 		
 		newState, cargo = BaseHandler.run(self, cargo)
 		
 		while cargo['p'] > 0:
-			cargo['last'] = current = infile.readline()
+			cargo['last'] = current = self.parent.infile.readline()
 			current = current.strip()
 			if '{' in current: cargo['p'] += 1
 			if '}' in current: cargo['p'] -= 1
-			if current.split()[0] in geosetkeys:
+			if current.split()[0] in self.parent.geosetkeys:
 				newState = current.split()[0].upper()
 				break
 	
@@ -132,27 +135,27 @@ class VERTICES(BaseHandler):
 	def run(self, cargo):
 		cargo = BaseHandler.run(self, cargo)[1]
 		for i in range(int(cargo['last'].strip().split()[1])):
-			current = infile.readline().strip().strip('{},;')
-			li = [float(n) for n in current.split(', ')]
-			geosets[cargo['geoindex']].vertices.append(li)
+			current = self.parent.infile.readline().strip().strip('{},;')
+			li = [(float(n)/20) for n in current.split(', ')]
+			self.parent.geosets[cargo['geoindex']].vertices.append(li)
 		return ['GEOSET', cargo]
 
 class NORMALS(BaseHandler):
 	def run(self, cargo):
 		cargo = BaseHandler.run(self, cargo)[1]
 		for i in range(int(cargo['last'].strip().split()[1])):
-			current = infile.readline().strip().strip('{},;')
+			current = self.parent.infile.readline().strip().strip('{},;')
 			li = [float(n) for n in current.split(', ')]
-			geosets[cargo['geoindex']].normals.append(li)
+			self.parent.geosets[cargo['geoindex']].normals.append(li)
 		return ['GEOSET', cargo]
 
 class TVERTICES(BaseHandler):
 	def run(self, cargo):
 		cargo = BaseHandler.run(self, cargo)[1]
 		for i in range(int(cargo['last'].strip().split()[1])):
-			current = infile.readline().strip().strip('{},:')
-			li = [float(n) for n in current.split(', ')]
-			geosets[cargo['geoindex']].tvertices.append(li)
+			current = self.parent.infile.readline().strip().strip('{},:')
+			li = [(float(n)/20.0) for n in current.split(', ')]
+			self.parent.geosets[cargo['geoindex']].tvertices.append(li)
 		return ['GEOSET', cargo]
 
 class FACES(BaseHandler):
@@ -161,36 +164,47 @@ class FACES(BaseHandler):
 		grps, cnt = [int(n) for n in cargo['last'].strip().split()[1:3]]
 		li = []
 		while len(li) < cnt:
-			cargo['last'] = current = infile.readline()
+			cargo['last'] = current = self.parent.infile.readline()
 			if current.strip().startswith('Triangles'):
 				for i in range(grps):
-					li += [int(n) for n in infile.readline().strip().strip('{},;').split(', ')]
+					li += [int(n) for n in self.parent.infile.readline().strip().strip('{},;').split(', ')]
+		if dbg: print(len(li))
 		for i in range(cnt//3):
-			geosets[cargo['geoindex']].faces.append([li[3*i], li[3*i+1], li[3*i+2]])
+			self.parent.geosets[cargo['geoindex']].faces.append([li[3*i], li[3*i+1], li[3*i+2]])
 		return ['GEOSET', cargo]
 
-def run(filepath):
-	global infile
-	print("Opening {}...".format(filepath))
-	infile = open(filepath, 'r')
-	m = StateMachine()
-	m.add('SEARCH', SEARCH, startState=True)
-	m.add('VERSION', VERSION)
-	m.add('GEOSET', GEOSET)
-	m.add('VERTICES', VERTICES)
-	m.add('NORMALS', NORMALS)
-	m.add('TVERTICES', TVERTICES)
-	m.add('FACES', FACES)
-	m.add('EOF', None, endState=True)
-	m.run()
+class DataImporter:
+	infile = None
+	globalkeys = ['Version', 'Geoset']
+	geosetkeys = ['Vertices', 'Normals', 'TVertices', 'Faces']
+	geosets = []
 	
-	# TODO: Actually use the gathered data
-	for geoset in geosets:
-		print('Vertices: {}'.format(geoset.vertices))
-		print('Normals: {}'.format(geoset.normals))
-		print('TVertices: {}'.format(geoset.tvertices))
-	
-	return {'FINISHED'}
+	def run(self, filepath, context):
+		print("Opening {}...".format(filepath))
+		self.infile = open(filepath, 'r')
+		m = StateMachine(parent=self)
+		m.add('SEARCH', SEARCH, startState=True)
+		m.add('VERSION', VERSION)
+		m.add('GEOSET', GEOSET)
+		m.add('VERTICES', VERTICES)
+		m.add('NORMALS', NORMALS)
+		m.add('TVERTICES', TVERTICES)
+		m.add('FACES', FACES)
+		m.add('EOF', None, endState=True)
+		m.run()
+		
+		# TODO: Actually use the gathered data
+		counter = 0
+		for geoset in self.geosets:
+			#if dbg: print('Vertices: {}'.format(geoset.vertices)); print('Normals: {}'.format(geoset.normals))#; print('TVertices: {}'.format(geoset.tvertices))
+			mesh = bpy.data.meshes.new("Geoset{}".format(counter))
+			mesh.from_pydata(geoset.vertices, [], geoset.faces)
+			mesh.update()
+			object_utils.object_data_add(context, mesh)
+			counter += 1
+			pdb.set_trace()
+		
+		return {'FINISHED'}
 
 class ImportWarMDL(bpy.types.Operator, ImportHelper):
 	'''Bla bla bla!'''
@@ -206,10 +220,11 @@ class ImportWarMDL(bpy.types.Operator, ImportHelper):
 	
 	@classmethod
 	def poll(cls, context):
-		return context.active_object is not None
+		return True
 	
 	def execute(self, context):
-		return run(self.filepath)
+		di = DataImporter()
+		return di.run(self.filepath, context)
 
 def menu_func_export(self, context):
 	self.layout.operator(ImportWarMDL.bl_idname, text="Import WarCraft MDL")
